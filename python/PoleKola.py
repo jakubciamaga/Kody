61e91c8178367806750570fcceca8e758d04d3b5
jakubciamaga <jakub.ciamaga@lo1.sandomierz.pl>
2020-09-11 09:11:39 +0200

    cdodanie plików źródłowych cpp i python

---
+++ python/PoleKola.py
@@ -0,0 +1,10 @@
+
+def main(args):
+
+    TwójStary = int(input("podaj obwód pasa starego"))
+    pole = TwójStary * TwójStary * 3.1415s
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
